2e03fa2cd6353fb0ccf9302c492a2aa70bd5ad78
artem-kostenko <artem.kostenko@pokupon.ua>
2013-06-04 11:48:29 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pramoda::Application.config.secret_token = 'a16e2be1d6859fc9c0069f788a546aff138799535a816f6794859c1aadc4b392acb8b60d8f8c23c90f7e281f7b28dce3737ffef1957b6e556aabdca593f58cf6'
